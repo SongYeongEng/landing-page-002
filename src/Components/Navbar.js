ab93cb49c90b2c8820d2dbabd4eb182331a765cc
Homura <esy.minmax@gmail.com>
2024-04-22 11:52:26 +0800

    Modify Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,12 +3,17 @@ import './Navbar.css'; // Import your CSS file for styling
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
+    <nav className='navbar'>
+      <div className='navbar-container'>
+        <div className="navbar-logo">
+        <a href="/">Logo</a>
+        </div>
+        <ul className='nav-menu'>
+          <li className='nav-item'><a href="#">Home</a></li>
+          <li className='nav-item'><a href="#">About</a></li>
+          <li className='nav-item'><a href="#">Contact</a></li>
+        </ul>
+        </div>
     </nav>
   );
 }
