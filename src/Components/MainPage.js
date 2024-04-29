d1ca34222a24f759791a93e50680654f0d99701c
Homura <esy.minmax@gmail.com>
2024-04-30 00:20:51 +0800

    adding animation update room

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,9 +1,11 @@
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three'; 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'; 
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'; 
 
 function MainPage() {
   const threeContainer = useRef();
+  let mixer;
 
   useEffect(() => {
     // Create a new scene
@@ -11,7 +13,7 @@ function MainPage() {
 
     // Create a new camera
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 15, 30); // Adjust camera position
+    camera.position.set(0, 5, 10); // Adjust camera position
     
     // Set the background color
     scene.background = new THREE.Color(0x4bb5eb); // Black color
@@ -38,13 +40,35 @@ function MainPage() {
       }
     );
 
+    // Load FBX model - Character
+    const characterLoader = new FBXLoader();
+    characterLoader.load(
+      process.env.PUBLIC_URL + '/models/walk.fbx', // Path to the character model
+      (character) => {
+        character.scale.set(0.05, 0.05, 0.05); // Adjust scale
+
+        scene.add(character); // Add the loaded character model to the scene
+
+        // Get animations
+        mixer = new THREE.AnimationMixer(character);
+        const action = mixer.clipAction(character.animations[1]); // Assuming the first animation
+        action.play();
+      },
+      undefined,
+      (error) => {
+        console.error('Error loading character FBX model', error);
+      }
+    );
+
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.position.set(1, 1, 1); // Adjust light position
     scene.add(directionalLight);
 
     // Animation loop
     const animate = () => {
-      scene.rotation.y += 0.01;
+      if (mixer) {
+        mixer.update(0.01); // Update mixer with delta time
+      }
       renderer.render(scene, camera);
       requestAnimationFrame(animate);
     };
