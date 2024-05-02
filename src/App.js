cff67d7e6dc4d39cabd229ee4ccf75188d722789
Homura <esy.minmax@gmail.com>
2024-05-02 20:08:28 +0800

    Companies Logo Alignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Companies from './Components/Companies';
 import MainPage from './Components/MainPage'; 
+import ShowCase from './Components/ShowCase';
 
 function App() {
   return (
     <div className="App">
       <MainPage />
       <Companies/>
+      <ShowCase/>
     </div>
 
     
--- src/Components/Companies.js
+++ src/Components/Companies.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Companies.css'; 
 import ExactLogo from './Exact_logo.svg';
+import BoatLogo from './boat.svg'; // Import the other PNG image
 
 function Companies() {
   return (
     <div className="companies">     
-     <svg className="svg-container" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+     <svg className="svg-container" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 75 75">
+     <image xlinkHref={BoatLogo} className="moving-image"  x="-300" />
      <image xlinkHref={ExactLogo} className="moving-image" />
      </svg>
     </div>
