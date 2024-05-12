516e8bd8dab5a831e4b3e4b69ffc52b500770955
Homura <esy.minmax@gmail.com>
2024-05-12 23:48:45 +0800

    Add burger menu later add responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'; // Import your CSS file for styling
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className='navbar'>
       <div className='navbar-container'>
         <div className="navbar-logo">
-        <img className="logo-img" src={process.env.PUBLIC_URL + '/images/Logo.svg'} alt="Example" />
+          <img className="logo-img" src={process.env.PUBLIC_URL + '/images/Logo.svg'} alt="Example" />
         </div>
-        <ul className='nav-menu'>
-          <li className='nav-item'><a href="#">Home</a></li>
-          <li className='nav-item'><a href="#">About</a></li>
-          <li className='nav-item'><a href="#">Contact</a></li>
-        </ul>
-        </div>
+        <button className="hamburger-menu" onClick={toggleMenu}>
+          <img className="burger-icon" src={process.env.PUBLIC_URL + '/images/Burger.svg'} alt="Menu" />
+        </button>
+        {isMenuOpen && (
+          <ul className="nav-menu">
+            <li className='nav-item'><a href="#">Home</a></li>
+            <li className='nav-item'><a href="#">About</a></li>
+            <li className='nav-item'><a href="#">Contact</a></li>
+          </ul>
+        )}
+      </div>
     </nav>
   );
 }
